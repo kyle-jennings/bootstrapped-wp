cd6136bea8975f8abe70adda5a64f441d7ff0774
rugbert <kjennings@gmail.com>
2014-07-01 08:12:22 -0400

    working on the mce shit

--- lib/admin/functions/shortcode-injector/window-header.php
+++ lib/admin/functions/shortcode-injector/window-header.php
@@ -28,3 +28,7 @@ require_once( WP_LOAD_PATH . '/wp-load.php');
 $adminRoot = dirname( dirname( dirname(__FILE__)));
 $site_root = get_bloginfo('template_directory');
 $site_url = get_option('siteurl');
+
+// check for rights
+if ( !is_user_logged_in() || !current_user_can('edit_posts') ) 
+	wp_die(__("You must be logged in to do that."));
