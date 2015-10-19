97ab573d34b2721276a42c95493142a901b33864
kyle <kyle@mobomo.com>
2015-10-18 22:24:03 -0400

    added footer fields

--- lib/admin/fields/section-settings-footer.php
+++ lib/admin/fields/section-settings-footer.php
@@ -24,7 +24,36 @@ $footer_settings = array(
     'tabs' => array(
         'settings'=>array(
             'label'=>'Settings',
-            'fields'=>array()
+            'fields'=>array(
+                'footer_height_toggle'=>select_field(array(
+                        'name'=>'footer_height_toggle',
+                        'label'=>'',
+                        'args'=>array('yes','no'),
+                        'toggle_field'=>array(
+                            'yes'=>array('footer_height'),
+                        )
+                    )
+                ),
+                    'footer_height'=>text_field(array(
+                            'name'=>'footer_height',
+                            'label'=>'',
+                            'args'=>array('suffix','px'),
+                             'toggled_field'=>array('footer_height_toggle','yes')
+                        )
+                    ),
+                'confine_section'=>select_field(array(
+                        'name'=>'confine_section',
+                        'label'=>'Confine Section',
+                        'args'=>array('yes','no')
+                    )
+                ),
+                'hide'=>select_field(array(
+                        'name'=>'hide',
+                        'label'=>'Hide',
+                        'args'=>array('none','frontpage','inside','all')
+                    )
+                ),
+            )
         ),
     )
 );
