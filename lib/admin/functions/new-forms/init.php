8a592b0fd4c8eb71840a0883ef30f10a7fede624
kyle <kyle@mobomo.com>
2015-10-30 15:20:29 -0400

    bs

--- lib/admin/functions/new-forms/init.php
+++ lib/admin/functions/new-forms/init.php
@@ -3,8 +3,8 @@
 $theme_root = get_template_directory();
 include($theme_root.'/lib/admin/functions/fields/class--bswpFields.php');
 
-$field_settings = new bswpFields;
-add_action('admin_init', array($field_settings, 'register_section_settings'));
+// $field_settings = new bswpFields;
+// add_action('admin_init', array($field_settings, 'register_section_settings'));
 
 // include($theme_root.'/lib/admin/functions/live-preview.php');
 include('class--bswpForm.php');
