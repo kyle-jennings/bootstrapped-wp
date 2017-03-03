3793b47295e185984a9b9daa9a138d01bdc1a798
kyle-jennings <kjennings@gmail.com>
2017-03-03 10:00:59 -0500

    toggled end color, auto dismiss notice

--- lib/admin/bswp/Settings/field-sets/background-colors.php
+++ lib/admin/bswp/Settings/field-sets/background-colors.php
@@ -19,19 +19,7 @@ $background_colors = array(
             'label'=>''
         )
     ),
-    'background_end_color'=>new ColorPicker(
-        array(
-            'name'=>'background_end_color',
-            'label'=>'End Color',
-            'args'=>'transparency'
-        )
-    ),
-    'background_end_color_rgba'=>new Hidden(
-        array(
-            'name'=>'background_end_color_rgba',
-            'label'=>''
-        )
-    ),
+
     'background_fill'=>new Select(
         array(
             'name'=>'background_fill',
@@ -43,6 +31,29 @@ $background_colors = array(
                 'horizontal',
                 'radial'
             ),
+            'toggle_fields' => array(
+                'solid' => 'background_end_color',
+                'vertical' => 'background_end_color',
+                'horizontal' => 'background_end_color',
+                'radial' => 'background_end_color',
+            )
+        )
+    ),
+
+    'background_end_color'=>new ColorPicker(
+        array(
+            'name'=>'background_end_color',
+            'label'=>'End Color',
+            'args'=>'transparency',
+            'toggled_by' => array(
+                'background_fill' => 'solid,vertical,horizontal,radial'
+            )
+        )
+    ),
+    'background_end_color_rgba'=>new Hidden(
+        array(
+            'name'=>'background_end_color_rgba',
+            'label'=>''
         )
     ),
 );
