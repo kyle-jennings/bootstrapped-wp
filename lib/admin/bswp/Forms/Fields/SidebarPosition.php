f5102fa54a776b5aff77606351e072508160d9d9
kyle-jennings <kjennings@gmail.com>
2017-03-29 18:48:35 -0400

    consolidated sidebar attribute settings

--- lib/admin/bswp/Forms/Fields/SidebarPosition.php
+++ lib/admin/bswp/Forms/Fields/SidebarPosition.php
@@ -15,7 +15,133 @@ class SidebarPosition extends Field{
     public function field_output() {
 
         $output = '';
-        $classes = $this->class . ' ' . $this->type .' js--sidebar-diagram-toggle';
+        $attrs = $this->set_attrs();
+        extract($attrs);
+
+        // the label
+        $output .= '<label>'.$this->label.'</label>';
+
+        $pos = $this->get_pos();
+        $vis= $this->get_vis();
+        // the select
+        $output .= '<select class="'.$classes.'" data-name="position">';
+            $output .= $this->options_markup($pos);
+        $output .= '</select>';
+
+        $output .= $this->diagram($pos);
+
+        $output .= $this->get_component_visibility($vis);
+
+        $output .= '<input
+            id="'.$id.'"
+            name="'.$field_name.'"
+            type="hidden"
+            value='.$value_str.'
+        />';
+
+        return $output;
+
+    }
+
+    private function get_pos()
+    {
+        foreach($this->value as $value) {
+            if($value->name == 'position')
+                return $value->value;
+        }
+    }
+
+
+    private function get_vis()
+    {
+        foreach($this->value as $value) {
+            if($value->name == 'visibility')
+                return $value->value;
+        }
+    }
+
+
+    private function options_markup($saved)
+    {
+        $output = '';
+        // loop through the args to set the select options
+        foreach ($this->args as $key=>$option):
+
+            $option_test = $option;
+            if(is_string($key))
+                $option_text = $key;
+
+            $name = strtolower(str_replace(' ','_',$option));
+            $data_targets = '';
+            $selected = $saved == $option ? 'selected' : '';
+            // the option markup
+            $output .= '<option value="'.$option.'" '.$selected.'>';
+                $output .= str_replace('_',' ',$option);
+            $output .= '</option>';
+
+        endforeach;
+
+        return $output;
+    }
+
+
+    private function get_component_visibility($vis)
+    {
+        $visibilities = array(
+            'all',
+            'visible_phone',
+            'visible_tablet',
+            'visible_desktop',
+            'hidden_phone',
+            'hidden_tablet',
+            'hidden_desktop',
+        );
+        $output = '<span class="sidebar-visibility">';
+            // $output .=  '<span class="dashicons dashicons-visibility"></span>';
+            $output .=  '<select class="js--sidebar-setting js--no-css-change" data-name="visibility">';
+                    foreach($visibilities as $v){
+                        $selected = $v == $vis ? 'selected' : '';
+                        $output .= '<option value="'.$v.'" '.$selected.'>';
+                            $output .= $this->clean_string($v);
+                        $output .= '</option>';
+                    }
+
+            $output .= '</select>';
+        $output .=  '</span>';
+
+        return $output;
+    }
+
+
+    private function diagram($value = 'none')
+    {
+
+        $output = '';
+        $output .=
+        '<div class="diagram js--diagram '.$value.'">
+            <div class="diagram-column diagram__content-area">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis, lectus eu pulvinar viverra, urna nisl varius magna, vel lacinia eros ipsum nec turpis. Integer convallis eros ut dictum auctor. Ut sagittis massa sit amet nibh tincidunt volutpat. Phasellus aliquet urna in leo aliquet, non ullamcorper arcu blandit. Nam nisi libero, tristique sed arcu vel, imperdiet ornare tortor. Vivamus sed nunc urna. Integer arcu elit, porttitor quis dapibus tempor, pulvinar ac velit.
+            </div>
+            <div class="diagram-column diagram__sidebar"></div>
+        </div>';
+
+        return $output;
+    }
+
+    private function value_str() {
+        $value = json_encode($this->value);
+        $value = str_replace('\\','', $value);
+        $value = trim($value, '"');
+        $value_str = htmlspecialchars($value);
+
+
+        $this->value = json_decode($this->value);
+
+        return $value_str;
+    }
+
+    public function set_attrs(){
+        $classes = $this->class . ' ' . $this->type .' js--sidebar-diagram-toggle js--no-css-change js--sidebar-setting';
         $classes .= $this->toggle_fields ? ' js--toggle-field' : '';
 
 
@@ -32,60 +158,23 @@ class SidebarPosition extends Field{
         $data_target_fields = $this->data_target_fields
         ? 'data-target_fields="'.$this->data_target_fields.'"' : '';
 
-        $value = isset($this->value) ? $this->value : '';
+        $value_str = $this->value_str();
 
         // the id and field name
         $id = $this->group_name.'-'.$this->tab_name.'-'.$this->name;
         $field_name = 'bswp_'.$this->section_name.
             '['.$this->group_name.']['.$this->tab_name.']['.$this->name.']';
 
-        // the label
-        $output .= '<label>'.$this->label.'</label>';
 
-
-        // the select
-        $output .= '<select class="'.$classes.'" id="'.$id.'" name="'.$field_name.'"
-            '.$data_field_name.'
-            '.$data_preview.'
-            '.$data_preview_deps.'
-            '.$data_target_fields.'>';
-
-        // loop through the args to set the select options
-        foreach ($this->args as $key=>$option):
-
-            $option_test = $option;
-            if(is_string($key))
-                $option_text = $key;
-
-            $name = strtolower(str_replace(' ','_',$option));
-            $data_targets = '';
-
-            // the option markup
-            $output .= '<option '.$data_targets.' value="'.$option.'" '.selected( $option, $this->value, false).'>';
-                $output .= str_replace('_',' ',$option);
-            $output .= '</option>';
-
-        endforeach;
-        $output .= '</select>';
-
-        $output .= $this->diagram($this->value);
-
-        return $output;
-
+        return array(
+            'id'=>$id,
+            'field_name' => $field_name,
+            'value_str' => $value_str,
+            'data_target_fields' => $data_target_fields,
+            'data_field_name' => $data_field_name,
+            'data_preview' => $data_preview,
+            'data_preview_deps' => $data_preview_deps,
+            'classes' => $classes
+        );
     }
-
-    public function diagram($value = 'none') {
-
-        $output = '';
-        $output .=
-        '<div class="diagram js--diagram '.$value.'">
-            <div class="diagram-column diagram__content-area">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis, lectus eu pulvinar viverra, urna nisl varius magna, vel lacinia eros ipsum nec turpis. Integer convallis eros ut dictum auctor. Ut sagittis massa sit amet nibh tincidunt volutpat. Phasellus aliquet urna in leo aliquet, non ullamcorper arcu blandit. Nam nisi libero, tristique sed arcu vel, imperdiet ornare tortor. Vivamus sed nunc urna. Integer arcu elit, porttitor quis dapibus tempor, pulvinar ac velit.
-            </div>
-            <div class="diagram-column diagram__sidebar"></div>
-        </div>';
-
-        return $output;
-    }
-
 }
--- lib/admin/bswp/Settings/components/sidebars.php
+++ lib/admin/bswp/Settings/components/sidebars.php
@@ -19,10 +19,10 @@ use function bswp\Settings\_helpers\border_settings_map;
 $layouts = new SettingsGroup('layouts');
 $layouts->add_tab('sidebars',
     array(
-        'frontpage_sidebar_location' => new SidebarPosition(
+        'frontpage_sidebar' => new SidebarPosition(
             array(
                 'label'=> 'Frontpage',
-                'name'=>'frontpage_sidebar_location',
+                'name'=>'frontpage_sidebar',
                 'args' => array(
                     'none',
                     'top',
@@ -30,39 +30,17 @@ $layouts->add_tab('sidebars',
                     'bottom',
                     'left',
                 ),
-                'toggle_fields' => array(
-                    'top'=>'frontpage_sidebar_visibility',
-                    'right'=>'frontpage_sidebar_visibility',
-                    'bottom'=>'frontpage_sidebar_visibility',
-                    'left'=>'frontpage_sidebar_visibility',
-                ),
+
                 'preview'=> null
             )
         ),
-        'frontpage_sidebar_visibility' => new Select(
-            array(
-                'label' => 'Sidebar Visibilty',
-                'name'=>'frontpage_sidebar_visibility',
-                'args' => array(
-                    'all',
-                    'visible_phone',
-                    'visible_tablet',
-                    'visible_desktop',
-                    'hidden_phone',
-                    'hidden_tablet',
-                    'hidden_desktop',
-                ),
-                'toggled_by'=> array(
-                    'frontpage_sidebar_location'=>'top,right,bottom,left'
-                )
-            )
-        ),
+
         'divider1'=>new Divider(),
 
-        'feed_sidebar_location' => new SidebarPosition(
+        'feed_sidebar' => new SidebarPosition(
             array(
                 'label'=> 'Feed',
-                'name'=>'feed_sidebar_location',
+                'name'=>'feed_sidebar',
                 'args' => array(
                     'none',
                     'top',
@@ -70,41 +48,16 @@ $layouts->add_tab('sidebars',
                     'bottom',
                     'left',
                 ),
-                'toggle_fields' => array(
-                    // 'basic'=>'brand,brand_image,position,movement,menu_toggle_type'
-                ),
-                'toggle_fields' => array(
-                    'top'=>'feed_sidebar_visibility',
-                    'right'=>'feed_sidebar_visibility',
-                    'bottom'=>'feed_sidebar_visibility',
-                    'left'=>'feed_sidebar_visibility',
-                ),
+
             )
         ),
-        'feed_sidebar_visibility' => new Select(
-            array(
-                'label' => 'Sidebar Visibilty',
-                'name'=>'feed_sidebar_visibility',
-                'args' => array(
-                    'all',
-                    'visible_phone',
-                    'visible_tablet',
-                    'visible_desktop',
-                    'hidden_phone',
-                    'hidden_tablet',
-                    'hidden_desktop',
-                ),
-                'toggled_by'=> array(
-                    'feed_sidebar_location'=>'top,right,bottom,left'
-                )
-            )
-        ),
+
         'divider2'=>new Divider(),
 
-        'single_sidebar_location' => new SidebarPosition(
+        'single_sidebar' => new SidebarPosition(
             array(
                 'label'=> 'Single',
-                'name'=>'single_sidebar_location',
+                'name'=>'single_sidebar',
                 'args' => array(
                     'none',
                     'top',
@@ -112,35 +65,11 @@ $layouts->add_tab('sidebars',
                     'bottom',
                     'left',
                 ),
-                'toggle_fields' => array(
-                    // 'basic'=>'brand,brand_image,position,movement,menu_toggle_type'
-                ),
-                'toggle_fields' => array(
-                    'top'=>'single_sidebar_visibility',
-                    'right'=>'single_sidebar_visibility',
-                    'bottom'=>'single_sidebar_visibility',
-                    'left'=>'single_sidebar_visibility',
-                ),
+
+
             )
         ),
-        'single_sidebar_visibility' => new Select(
-            array(
-                'label' => 'Sidebar Visibilty',
-                'name'=>'single_sidebar_visibility',
-                'args' => array(
-                    'all',
-                    'visible_phone',
-                    'visible_tablet',
-                    'visible_desktop',
-                    'hidden_phone',
-                    'hidden_tablet',
-                    'hidden_desktop',
-                ),
-                'toggled_by'=> array(
-                    'single_sidebar_location'=>'top,right,bottom,left'
-                )
-            )
-        ),
+
 
     )
 );
